fe1921c1c762a7bfb8b06a2aa2d04ebb6042e85b
kartheek <kartheekdivvela@gmail.com>
2022-01-26 21:19:49 +0530

    removed all bugs and styled for good looking

--- main.js
+++ main.js
@@ -6,13 +6,28 @@ let rep = 3;
 let colclass = "col-" + 4;
 let completed = [0, 0, 0];
 
+function generateOutput() {
+    let a = document.createElement("h1");
+    let b = document.createElement("button");
+    b.classList.add("btn");
+    b.classList.add("ansbtn");
+    a.classList.add("congrats");
+    b.textContent = "Try Again"
+    b.onclick = function() {
+        initiate();
+    }
+    a.textContent = "Hurray! You guessed it right";
+    document.getElementById("result").appendChild(a);
+    document.getElementById("result").appendChild(b);
+}
+
 function generateBlock(r, g, b, i) {
     let out = document.createElement("div");
     out.classList.add(colclass);
     let cb = document.createElement("div");
     cb.classList.add("color-block");
     cb.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
-    cb.textContent = "hello";
+    //cb.textContent = "hello";
     out.appendChild(cb);
     out.onclick = function() {
         if(completed[i] === 1) {
@@ -22,13 +37,23 @@ function generateBlock(r, g, b, i) {
         pg.classList.add("opt-ans");
         pg.textContent = "RGB(" + r + ", " + g + ", " + b + ")";
         completed[i] = 1;
+        let flag = false;
+        if(r == qr && g == qg && b == qb) {
+            pg.classList.add("success");
+            flag = true;
+        }
+        else {
+            pg.classList.add("failed");
+        }
         out.appendChild(pg);
+        if(flag) {
+            generateOutput();
+        }
     }
     return out;
 }
 
 function getRandomColors() {
-    console.log(qr, qg, qb);
     let red = qr;
     while(red === qr) {
         red = parseInt(Math.random() * 1000) % 256;
@@ -61,38 +86,55 @@ function setQuestion() {
 function initiate() {
     setQuestion();
     document.getElementById("colorsRow").innerHTML = "";
+    document.getElementById("result").innerHTML = "";
     completed = [];
     for(let i = 0; i < rep; i++) {
         completed.push(0);
     }
     let anspos = parseInt(Math.random() * 10) % rep;
-    console.log(anspos);
     for(let i = 0; i < rep; i++) {
         if(i === anspos) {
-            let bl = generateBlock(qr, qg, qb);
-            console.log(bl);
+            let bl = generateBlock(qr, qg, qb, i);
             document.getElementById("colorsRow").appendChild(bl);
         }
         else {
             let rc = getRandomColors();
             let bl = generateBlock(rc.r, rc.g, rc.b, i);
-            console.log(bl);
             document.getElementById("colorsRow").appendChild(bl);
         }
     }
-    console.log(document.getElementById("colorsRow"));
 }
-
-document.getElementById("ebutton").onclick = function() {
+let ebt = document.getElementById("ebutton");
+let hbt = document.getElementById("hbutton");
+ebt.onclick = function() {
     difficulty = "e";
     colclass = "col-" + 4;
     rep = 3;
+    console.log("clicked on easy button");
+    if(!ebt.classList.contains("active")) {
+        ebt.classList.add("active");
+        console.log("adding active status to easy");
+    }
+    if(hbt.classList.contains("active")) {
+        hbt.classList.toggle("active");
+        console.log("removing active status to hard");
+    }
     initiate();
 };
-document.getElementById("hbutton").onclick = function() {
+hbt.onclick = function() {
     difficulty = "h";
     colclass = "col-" + 2;
     rep = 6;
+    console.log("clicked on hard");
+    if(!hbt.classList.contains("active")) {
+        hbt.classList.add("active");
+        console.log("adding active to hard");
+    }
+    console.log("class list of easy is" + ebt.classList);
+    if(ebt.classList.contains("active")) {
+        ebt.classList.toggle("active");
+        console.log("removing active status to easy");
+    }
     initiate();
 };
 initiate();
